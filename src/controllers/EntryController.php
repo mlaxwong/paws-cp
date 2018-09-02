e1e7fe503a8be26e4fdd7b511c2269c93cffa226
Mlax Wong <mlaxwong@gmail.com>
2018-09-02 14:03:20 +0800

    working on entry

--- src/controllers/EntryController.php
+++ src/controllers/EntryController.php
@@ -3,13 +3,39 @@ namespace paws\cp\controllers;
 
 use yii\web\Controller;
 use paws\models\Entry;
+use paws\records\EntryType;
 
 class EntryController extends Controller
 {
     public function actionIndex()
     {
-        $test = new Entry(1);
-        
+        $testing = new Entry(['entry_type_id' => 1]);
+        $testing->title = 'new one';
+        echo $testing->save() ? 'yes' : 'no';
+
+        echo $testing->id;
+
+        // $test = Entry::find()->type(1)->one();
+        // echo $test->title;
+        // $new = new Entry(['entry_type_id' => 1]);
+        // $new->refresh();
+
+        // $reflection = new \ReflectionClass($new);
+        // $method = $reflection->getMethod($methodName);
+        // $method->setAccessible(true);
+        // return $method->invokeArgs($object, $parameters);
+        // $property = $reflection->getProperty('_attributes');
+        // $property->setAccessible(true);
+        // echo '<pre>';print_r($property->getValue($new));
+        // echo '<pre>';print_r($new->attributes());
+        // $new->entry_type_id = 1;
+        // $new->title = 'haha';
+        // echo $new->title;
+        // echo $new->save() ? 'yes' : 'no';
+        // echo '<br />';
+        // print_r($new->errors);
+        die;
+
         return $this->render('list');
     }
 
